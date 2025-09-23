db523a9afe101bd8077547c87ba320be8fa68e97
sandervanvugt <mail@sandervanvugt.nl>
2025-09-23 13:28:39 +0200

    message

--- exam/exam-task6.sh
+++ exam/exam-task6.sh
@@ -1,29 +1,17 @@
-export VGUSEDBYMYFILES=$(lvdisplay | grep -A3 myfiles | awk '/VG Name/ { print $3 }')
-export VGUSEDBYROOT=$(lvdisplay | grep -A3 root | awk '/VG Name/ { print $3 }')
-
-if vgdisplay $(echo VGUSEDBYMYFILES | grep 'PE Size' | grep '8.00' &>/dev/null
+if ssh server2 getent chisha | grep 1234 &>/dev/null && ssh server2 getent myapp | grep nologin &>/dev/null
 then
-	echo -e "\033[32m[OK]\033[0m\t\t the VG used by the LV myfiles uses 8 MiB extents"
+	echo -e "\033[32m[OK]\033[0m\t\t users chisha and myapp were correctly created"
 	SCORE=$(( SCORE + 10 ))
 else
-	echo -e "\033[31m[FAIL]\033[0m\t\t the VG used by the LV myfiles does not use 8MiB extents"
+	echo -e "\033[31m[FAIL]\033[0m\t\t users chisha and myapp were not correctly created"
 fi
 TOTAL=$(( TOTAL + 10 ))
 
-if grep myfiles.*/mnt/data /etc/fstab &>/dev/null
+if grep sales /etc/group | grep chisha | grep caroline &>/dev/null
 then
-	echo -e "\033[32m[OK]\033[0m\t\t the \033[1mmyfiles\033[0m LV mounts on /mnt/data"
+	echo -e "\033[32m[OK]\033[0m\t\t group sales was found with correct membership"
 	SCORE=$(( SCORE + 10 ))
 else
-	echo -e "\033[31m[FAIL]\033[0m\t\t the \033[1mmyfiles\033[0m LV does not mount on /mnt/data"
-fi
-TOTAL=$(( TOTAL + 10 ))
-
-if vgdisplay | grep -A 15 $VGUSEDBYROOT | grep 'Cur PV.*2' &>/dev/null
-then
-	echo -e "\033[32m[OK]\033[0m\t\t the root logical volume \033[1mlvlabs\033[0m was resized"
-	SCORE=$(( SCORE + 10 ))
-else
-	echo -e "\033[31m[FAIL]\033[0m\t\t the root logical volume was not resized"
+	echo -e "\033[31m[FAIL]\033[0m\t\t group sales with correct membership was not found"
 fi
 TOTAL=$(( TOTAL + 10 ))
