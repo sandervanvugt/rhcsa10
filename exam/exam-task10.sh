764b5772a131cd8dae3b17752fc2b027f21e608d
sandervanvugt <mail@sandervanvugt.nl>
2025-09-24 14:39:50 +0200

    message

--- exam/exam-task10.sh
+++ exam/exam-task10.sh
@@ -1,58 +1,27 @@
-if grep 'PASS_MAX_DAYS.*90' /etc/login.defs &>/dev/null
+if showmount -e server2 | grep homes &>/dev/null
 then
-	echo -e "\033[32m[OK]\033[0m\t\t password validity set to 90 days"
+	echo -e "\033[32m[OK]\033[0m\t\t NFS shared directory /homes offered by server2"
 	SCORE=$(( SCORE + 10 ))
 else
-	echo -e "\033[31m[FAIL]\033[0m\t\t password validity not set to 90 days"
+	echo -e "\033[31m[FAIL]\033[0m\t\t NFS shared directory /homes not offered by server2"
 fi
-#TOTAL=$(( TOTAL + 10 ))
+TOTAL=$(( TOTAL + 10 ))
 
-
-if [ -f /etc/skel/newfile ] &>/dev/null
+export AUTOFSFILE=$(awk '/homes/ { print $2 }' /etc/auto.master)
+if grep '\*.*/homes/&' $AUTOFSFILE &>/dev/null
 then
-	echo -e "\033[32m[OK]\033[0m\t\t empty file newfile will be created in new user homedirectories"
+	echo -e "\033[32m[OK]\033[0m\t\t wildcard mount correctly configured"
 	SCORE=$(( SCORE + 10 ))
 else
-	echo -e "\033[31m[FAIL]\033[0m\t\t no empty file newfile will be created in new user homedirectories"
+	echo -e "\033[31m[FAIL]\033[0m\t\t wildcard mount not correctly configured"
 fi
-#TOTAL=$(( TOTAL + 10 ))
+TOTAL=$(( TOTAL + 10 ))
 
-
-if grep -q 'anna' /etc/passwd && grep -q 'anouk' /etc/passwd && grep -q 'linda' /etc/passwd && grep -q 'lisa' /etc/passwd &>/dev/null
+if systemctl is-enabled autofs &>/dev/null
 then
-	echo -e "\033[32m[OK]\033[0m\t\t all requested users exist"
+        echo -e "\033[32m[OK]\033[0m\t\t the autofs service is enabled"
 	SCORE=$(( SCORE + 10 ))
 else
-	echo -e "\033[31m[FAIL]\033[0m\t\t one or more requested users does not exist"
+        echo -e "\033[31m[FAIL]\033[0m\t\t the autofs service is not enabled"
 fi
-#TOTAL=$(( TOTAL + 10 ))
-
-
-if lid -g profs | grep anna &>/dev/null && lid -g profs | grep anouk &>/dev/null && lid -g students linda &>/dev/null && lid -g students lisa &>/dev/null
-then
-	echo -e "\033[32m[OK]\033[0m\t\t the users are all member of the requested groups"
-	SCORE=$(( SCORE + 10 ))
-else
-	echo -e "\033[31m[FAIL]\033[0m\t\t one or more users is not in the right secondary groups"
-fi
-#TOTAL=$(( TOTAL + 10 ))
-
-
-if test -d /data/profs && test -d /data/students &>/dev/null
-then
-	echo -e "\033[32m[OK]\033[0m\t\t the directories /data/profs and /data/students exist"
-	SCORE=$(( SCORE + 10 ))
-else
-	echo -e "\033[31m[FAIL]\033[0m\t\t at least one of the directories /data/profs and /data/students doesn't exist"
-fi
-#TOTAL=$(( TOTAL + 10 ))
-
-
-if ls -ld /data/* | grep 'd...rwx---' &>/dev/null
-then
-	echo -e "\033[32m[OK]\033[0m\t\t shared group directories permissions set correctly"
-	SCORE=$(( SCORE + 10 ))
-else
-	echo -e "\033[31m[FAIL]\033[0m\t\t shared group directories permissions not set correctly"
-fi
-#TOTAL=$(( TOTAL + 10 ))
+TOTAL=$(( TOTAL + 10 ))
