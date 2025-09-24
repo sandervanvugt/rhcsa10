fd0db448126e4cf04c87daa8eca0f7b9863d64cd
sandervanvugt <mail@sandervanvugt.nl>
2025-09-24 14:09:35 +0200

    message

--- exam/exam-task8.sh
+++ exam/exam-task8.sh
@@ -1,27 +1,27 @@
-if [ -f /etc/systemd/system/sleep.service ] &>/dev/null
+if ssh server2 test -f /home/chisha/.config/systemd/user/*timer &>/dev/null
 then
-	echo -e "\033[32m[OK]\033[0m\t\t sleep.service found in the right location"
+	echo -e "\033[32m[OK]\033[0m\t\t a systemd timer file was found in the user homedirectory"
 	SCORE=$(( SCORE + 10 ))
 else
-	echo -e "\033[31m[FAIL]\033[0m\t\t sleep.service not found in the right location"
+	echo -e "\033[31m[FAIL]\033[0m\t\t a systemd timer file was not found in the user homedirectory"
 fi
-#TOTAL=$(( TOTAL + 10 ))
+TOTAL=$(( TOTAL + 10 ))
 
-if grep 'WantedBy=multi-user.target' /etc/systemd/system/sleep.service &>/dev/null
+if ssh server2 grep 'OnCalendar=Mon..Fri *-*-* 02:00:00' /home/chisha/.config/systemd/*timer &>/dev/null
 then
-	echo -e "\033[32m[OK]\033[0m\t\t \033[1msleep.service\033[0m is enabled"
+	echo -e "\033[32m[OK]\033[0m\t\t the timer is scheduled for the correct time"
 	SCORE=$(( SCORE + 10 ))
 else
-	echo -e "\033[31m[FAIL]\033[0m\t\t \033[1msleep.service\033[0m is not enabled"
+	echo -e "\033[31m[FAIL]\033[0m\t\t the timer is not scheduled for the correct time"
 fi
-#TOTAL=$(( TOTAL + 10 ))
+TOTAL=$(( TOTAL + 10 ))
 
-if grep 'Restart=always' /etc/systemd/system/sleep.service &>/dev/null
+if ssh server2 test -f /home/chisha/.config/systemd/user/default.target.wants/*timer &>/dev/null
 then
-	echo -e "\033[32m[OK]\033[0m\t\t service will automatically restart"
+	echo -e "\033[32m[OK]\033[0m\t\t the timer service is enabled for the user"
 	SCORE=$(( SCORE + 10 ))
 else
-	echo -e "\033[31m[FAIL]\033[0m\t\t Restart option not found"
+	echo -e "\033[31m[FAIL]\033[0m\t\t the timer service is not enabled for the user"
 fi
-#TOTAL=$(( TOTAL + 10 ))
+TOTAL=$(( TOTAL + 10 ))
 
