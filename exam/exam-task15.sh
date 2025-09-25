f1c6fd26112ae87d18153f75ef3d5551f5210017
sandervanvugt <mail@sandervanvugt.nl>
2025-09-25 09:59:21 +0200

    message

--- exam/exam-task15.sh
+++ exam/exam-task15.sh
@@ -2,7 +2,7 @@
 find /etc -type f -readable -print0 | xargs -0 -r grep -iH 'administrator' > /root/lab15.check
 
 
-if diff -u <(sort /root/lab15.check) <(/root/administratorfiles.txt) &>/dev/null
+if diff -u <(sort /root/lab15.check) <(sort /root/administratorfiles.txt) &>/dev/null
 then
 	echo -e "\033[32m[OK]\033[0m\t\t you correctly identified all files in /etc that contain the text administrator"
 	SCORE=$(( SCORE + 10 ))
