d7eb0cfde25d4c8ef38478387b0727c77df4a889
sandervanvugt <mail@sandervanvugt.nl>
2025-09-25 09:17:43 +0200

    message

--- exam/exam-task9.sh
+++ exam/exam-task9.sh
@@ -1,14 +1,14 @@
 if ssh server2 "runuser -u chisha -- flatpak list" | grep -i gimp &>/dev/null
 then
 	echo -e "\033[32m[OK]\033[0m\t\t user chisha has gimp installed"
-	if ssh server2 "runuser -u anna  -- flatpak list" | grep -i gimp" &>/dev/null
+	if ssh server2 "runuser -u anna  -- flatpak list" | grep -i gimp &>/dev/null
 	then
-		echo -e "\033[31m[FAIL]\033[0m\t\t user anna also has access to the gimp flatpak and that's wrong"
+		echo -e "\033[31m[FAIL]\033[0m\t\t user anna also has access to the gimp flatpak and thats wrong"
 	else	
 		echo -e "\033[32m[OK]\033[0m\t\t user anna has no access to the gimp flatpak"
 		SCORE=$(( SCORE + 10 ))
 	fi
 else
-	echo -e "\033[31m[WARNING]\033[0m\t\t user chisha does not have gimp installed"
+	echo -e "\033[31m[FAIL]\033[0m\t\t user chisha does not have gimp installed"
 fi
 TOTAL=$(( TOTAL + 10 ))
