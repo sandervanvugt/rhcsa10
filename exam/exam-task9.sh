98088607b9c23feca8151402528335e1a9852e35
sandervanvugt <mail@sandervanvugt.nl>
2025-09-25 09:16:24 +0200

    message

--- exam/exam-task9.sh
+++ exam/exam-task9.sh
@@ -1,7 +1,7 @@
-if ssh server2 su - chisha "flatpak list --user | grep -i gimp" &>/dev/null
+if ssh server2 "runuser -u chisha -- flatpak list" | grep -i gimp &>/dev/null
 then
 	echo -e "\033[32m[OK]\033[0m\t\t user chisha has gimp installed"
-	if ssh server2 sudo - anna "flatpak list --user | grep -i gimp" &>/dev/null
+	if ssh server2 "runuser -u anna  -- flatpak list" | grep -i gimp" &>/dev/null
 	then
 		echo -e "\033[31m[FAIL]\033[0m\t\t user anna also has access to the gimp flatpak and that's wrong"
 	else	
