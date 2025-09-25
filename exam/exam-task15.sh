b5996e9ee465244ecad720a6232ebfd1890e5962
sandervanvugt <mail@sandervanvugt.nl>
2025-09-25 08:12:38 +0200

    message

--- exam/exam-task15.sh
+++ exam/exam-task15.sh
@@ -1,2 +1,10 @@
+grep -Ri 'administrator' /etc/* > /tmp/lab15.check &>/dev/null
 
-####checking the systemd parts
+if diff /tmp/lab15.check /root/administratorfiles.txt &>/dev/null
+then
+	echo -e "\033[32m[OK]\033[0m\t\t you correctly identified all files in /etc that contain the text administrator"
+	SCORE=$(( SCORE + 10 ))
+else
+	echo -e "\033[31m[WARNING]\033[0m\t\t you did not correctly identify all files in /etc that contain the text administrator"
+fi
+TOTAL=$(( TOTAL + 10 ))
