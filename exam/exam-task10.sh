ed493b6c612c2b1e87dbe8dd78de9e4d81e248e7
sandervanvugt <mail@sandervanvugt.nl>
2025-09-26 08:41:31 +0200

    message

--- exam/exam-task10.sh
+++ exam/exam-task10.sh
@@ -1,19 +1,29 @@
-if showmount -e server2 | grep homes &>/dev/null
+if $(which showmount) &>/dev/null
 then
-	echo -e "\033[32m[OK]\033[0m\t\t NFS shared directory /homes offered by server2"
-	SCORE=$(( SCORE + 10 ))
+	if showmount -e server2 | grep homes &>/dev/null
+	then
+		echo -e "\033[32m[OK]\033[0m\t\t NFS shared directory /homes offered by server2"
+		SCORE=$(( SCORE + 10 ))
+	else
+		echo -e "\033[31m[FAIL]\033[0m\t\t NFS shared directory /homes not offered by server2"
+	fi
 else
-	echo -e "\033[31m[FAIL]\033[0m\t\t NFS shared directory /homes not offered by server2"
+	echo -e "\033[31m[FAIL]\033[0m\t\t the nfs-utils package doesnt seem to be installed"
 fi
 TOTAL=$(( TOTAL + 10 ))
 
-export AUTOFSFILE=$(awk '/homes/ { print $2 }' /etc/auto.master)
-if grep '\*.*/homes/&' $AUTOFSFILE &>/dev/null
+if [ -f /etc/auto.master ] &>/dev/null
 then
-	echo -e "\033[32m[OK]\033[0m\t\t wildcard mount correctly configured"
-	SCORE=$(( SCORE + 10 ))
+	export AUTOFSFILE=$(awk '/homes/ { print $2 }' /etc/auto.master)
+	if grep '\*.*/homes/&' $AUTOFSFILE &>/dev/null
+	then
+		echo -e "\033[32m[OK]\033[0m\t\t wildcard mount correctly configured"
+		SCORE=$(( SCORE + 10 ))
+	else
+		echo -e "\033[31m[FAIL]\033[0m\t\t wildcard mount not correctly configured"
+	fi
 else
-	echo -e "\033[31m[FAIL]\033[0m\t\t wildcard mount not correctly configured"
+	echo -e "\033[31m[FAIL]\033[0m\t\t /etc/auto.master does not exist"
 fi
 TOTAL=$(( TOTAL + 10 ))
 
